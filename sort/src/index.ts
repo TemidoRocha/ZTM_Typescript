c2cd1a42d192598e4215abc7a9de20cfbeccc8df
TemidoRocha <manueltemidorocha@gmail.com>
2022-01-25 21:02:39 +0000

    #sort #concurrently
    "start:build": "tsc -w",
    "start:run": nodemon build/index.js",
    "start": "concurrently npm:start:*"

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+/**
+ * tsc -w => will watch for updates and compile the TS into JS
+ *
+ * concurrently will help the compilation and serving
+ */
+
+const logSth = () => {
+  console.log('hello');
+};
