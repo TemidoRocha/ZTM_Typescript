918cd493969c556976e710ec22a6f11ec48ace3a
TemidoRocha <manueltemidorocha@gmail.com>
2022-01-26 21:19:54 +0000

    #sort #implementation of LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -10,6 +10,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([-10000, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -20,3 +21,14 @@ const charactersCollection = new CharactersCollection('Xfqochow');
 const _sort = new Sorter(charactersCollection);
 _sort.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(53);
+linkedList.add(-450);
+linkedList.add(-5);
+linkedList.add(6540);
+
+const __sorter = new Sorter(linkedList);
+__sorter.sort();
+linkedList.print();
