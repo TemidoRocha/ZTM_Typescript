7e80d0402e54ef76b4e3cfc9a063d72349c66dea
TemidoRocha <manueltemidorocha@gmail.com>
2022-01-27 16:48:44 +0000

    #stats #Enum

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -12,4 +12,28 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+/**
+ * enum - enumeration
+ *
+ * 1. Follow near-identical syntax rules as normal objects
+ * 2. Creates an object with the same keys and values when converted from TS to JS
+ * 3. Primary goal is to signal to other engineers that theses are all closely related valeus
+ * 4. Use whenever we have a small fixed set of values that are all closely related and known at compile time
+ */
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
