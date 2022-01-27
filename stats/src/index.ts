38422a52b5e0b10bdcaac4c7937e5030a186e8db
TemidoRocha <manueltemidorocha@gmail.com>
2022-01-27 07:14:18 +0000

    #stats #load data with fs

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello');
+import fs from 'fs';
+/**
+ * even for node js we need to install the type definition files
+ * for any library inside node we have only one type definition file
+ * npm i @types/nodes
+ */
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+console.log(matches);
