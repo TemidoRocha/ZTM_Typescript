1ea96013ebe6696c64cd1bc77a9a066388ac6c83
TemidoRocha <manueltemidorocha@gmail.com>
2022-02-13 09:46:58 +0000

    #web #Building a framework
    Final User refactor

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,7 +1,7 @@
-import { AxiosResponse } from 'axios';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
 
 export interface UserProps {
   id?: number;
@@ -11,58 +11,12 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  /**
-   * hard code dependencies as class p+roperties
-   * in this case it makes sense because this eventing will hardly change
-   * we lose one of the big strenghts of composition because we cannot swap eventing with other class
-   */
-  public attributes: Attributes<UserProps>;
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  // the idea is not to call the function but to return the reference to the events.on
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
