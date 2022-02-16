93f02500096606dcfbcded5ea6bf97f8e62b9392
TemidoRocha <manueltemidorocha@gmail.com>
2022-02-16 22:09:10 +0000

    #web #Building a framework
    Nesting Views

--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -1,12 +1,22 @@
 import { Model } from '../models/Model';
 
 export abstract class View<T extends Model<K>, K> {
+  // regions is an element that we want to nest inside the view
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
-  abstract eventsMap(): { [key: string]: () => void };
   abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => this.render());
@@ -24,6 +34,19 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     // empty out the parent Element
     this.parent.innerHTML = '';
@@ -32,6 +55,10 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    // set the view nesting before append anything to the DOM
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
