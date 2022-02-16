9fa0ccecc8d070599aafaf845e460e36d42e2ec2
TemidoRocha <manueltemidorocha@gmail.com>
2022-02-16 17:08:43 +0000

    #web #Building a framework
    Re-rendering on Model Change

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -12,12 +12,21 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
   // it is important to be an arrow funtion in order to bind
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    const name = input.value;
+
+    this.model.set({ name });
   };
 
   template(): string {
@@ -27,7 +36,7 @@ export class UserForm {
         <div>User name: ${this.model.get('name')}</div>
         <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
