c0e368d828943abe7538f74df918572907946b17
TemidoRocha <manueltemidorocha@gmail.com>
2022-02-27 09:53:03 +0000

    #server #Decorators

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,12 +1,12 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-import { get, controller } from './decorators';
+import { get, controller, bodyValidator, post } from './decorators';
 
 @controller('/auth')
 class LoginController {
+  //
   @get('/login')
   getLogin(req: Request, res: Response): void {
-    console.log('hell');
     res.send(`
     <form method="POST">
       <div>
@@ -20,5 +20,26 @@ class LoginController {
       <button>Submit</button>
     </form>
   `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'passwprd')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'pw') {
+      // mark this person as logged input
+      req.session = { loggedIn: true };
+      // redirect tehm to the root
+      res.redirect('/');
+    } else {
+      res.status(422).send('You need to provide an email');
+    }
+  }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect('/');
   }
 }
--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -2,19 +2,52 @@ import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { NextFunction, RequestHandler, Request, Response } from 'express';
 
+function bodyValidators(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send(`MIssing property ${key}`);
+        return;
+      }
+    }
+
+    next();
+  };
+}
+
+//controller factory decorator
+//takes in a prefix of the route, can be "auth" "login" etc
 export function controller(routePrefix: string) {
+  //takes in a target
   return function (target: Function) {
+    //makes or get the instance of the router
     const router = AppRouter.getInstance();
 
     for (let key in target.prototype) {
+      //gets the routeHandler to pass into the router at the bottom
       const routeHandler = target.prototype[key];
+      //gets the path from the metadata to pass into the router
       const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
+      //gets the method from the metadata
       const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
+      //makes an array of middlewares to get them all together
       const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
+      //gathers the bodyProps that are required for validation
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
+
+      //runs the bodyValidator with the required props
+      const validator = bodyValidators(requiredBodyProps);
 
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, validator, routeHandler);
       }
     }
   };
